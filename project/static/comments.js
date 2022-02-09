a189666afef0e12adc166b759d491fad77ebc5c1
Lazar Miladinovic <lmiladinovic2318rn@raf.rs>
2022-02-09 23:29:09 +0100

    commit

--- project/static/comments.js
+++ project/static/comments.js
@@ -3,7 +3,7 @@ function init() {
     const cookies = document.cookie.split('=');
     const token = cookies[cookies.length - 1];
 
-    fetch('http://127.0.0.1:8088/api/comments')
+    fetch('p')
         .then( res => res.json() )
         .then( data => {
             const lst = document.getElementById('commentsList');
